a154dab6ba31efc3ebd45488b496cc5429689c28
Stepan Pyzhov <32341341+spyzhov@users.noreply.github.com>
2020-01-09 18:25:53 +0100

    Simplify PHP-7.4 examples (#2)

--- php-7.4/extension/libimgutil.php
+++ php-7.4/extension/libimgutil.php
@@ -8,21 +8,24 @@ class ImgUtil
     {
         if (is_null(self::$ffi)) {
             self::$ffi = \FFI::cdef("
-typedef long GoInt;
-extern char* ImgutilGetImageSize(char* p0, GoInt* p1, GoInt* p2);
+typedef unsigned long long GoUint;
+extern char* ImgutilGetImageSize(char* p0, GoUint* p1, GoUint* p2);
 ", __DIR__ . "/libimgutil.so");
         }
     }
 
     public function imgutil(string $path): int
     {
-        $h = \FFI::new("long");
-        $w = \FFI::new("long");
+        $h = \FFI::new("unsigned long long");
+        $w = \FFI::new("unsigned long long");
         $err = self::$ffi->ImgutilGetImageSize($path, \FFI::addr($w), \FFI::addr($h));
         if ($err) {
             printf("ERROR: %s", (string)$err);
             return 100;
         }
-        return (int)printf("%s: %lux%lu\n", $path, $w->cdata, $h->cdata);
+
+        printf("%s: %lux%lu\n", $path, $w->cdata, $h->cdata);
+
+        return 0;
     }
 }
--- php-7.4/test.php
+++ php-7.4/test.php
@@ -1,7 +1,5 @@
 <?php
-if (!class_exists('ImgUtil', false)) {
-    require_once __DIR__. '/extension/preload.php';
-}
+require_once __DIR__ . "/extension/libimgutil.php";
 $util = new ImgUtil();
 echo str_repeat("=", 20) . "\n";
 var_dump($util->imgutil('icon_128.png'));
